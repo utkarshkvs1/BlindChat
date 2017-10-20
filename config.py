43885c3bd89854ea9b9d5e5ab7e0138c3487bc7b
Your Name <you@example.com>
2017-10-20 23:22:09 +0530

    added runtime.txt and updated config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database setup
-SQLALCHEMY_DATABASE_URI = 'YOUR_DATABASE_URI'
+SQLALCHEMY_DATABASE_URI = 'postgres://bfpxfnoptnjkka:34673f26d35fc1cb5ad131dcd045126bf5d7058a212f8a0319ca0773c8f27588@ec2-54-243-43-72.compute-1.amazonaws.com:5432/dcokktksp4ntae'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -11,7 +11,7 @@ ACCESS_TOKEN = 'YOUR_FACEBOOK_APP_ACCESS_TOKEN'
 VERIFY_TOKEN = 'FACEBOOK_APP_VERIFY_TOKEN'
 
 # App
-APP_URL = "https://embeeblindchat.herokuapp.com/"
+APP_URL = "https://blindchat9211.herokuapp.com/"
 
 # For analytics purposes
 CHATMETRICS_TOKEN = 'CHATMETRICS_TOKEN'
